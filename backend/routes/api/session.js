6a954c4b8d9958174a345057fdaf1094203ae1e7
Sean Baeyens <srbaeyens@gmail.com>
2023-01-20 15:19:16 -0800

    phase 4 complete

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -28,7 +28,14 @@ router.delete("/", (_req, res) => {
   return res.json({ message: "success" });
 });
 
-
+router.get("/", restoreUser, (req, res) => {
+  const { user } = req;
+  if (user) {
+    return res.json({
+      user: user.toSafeObject(),
+    });
+  } else return res.json({ user: null });
+});
 
 
 module.exports = router;
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -19,4 +19,6 @@ router.post(
 );
 
 
+
+
 module.exports = router;
