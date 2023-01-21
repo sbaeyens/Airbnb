9884bb467288ba46a6a10392b6486cc435ad3ea6
Sean Baeyens <srbaeyens@gmail.com>
2023-01-20 16:56:57 -0800

    edit to migration

--- backend/db/migrations/20230119203746-create-user.js
+++ backend/db/migrations/20230119203746-create-user.js
@@ -59,6 +59,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Users";
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('Users', options);
   }
 };
