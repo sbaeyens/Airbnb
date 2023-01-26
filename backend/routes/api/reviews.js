cf61a8aa474f23f625b0a3dbab9264233bdaf2c4
Sean Baeyens <srbaeyens@gmail.com>
2023-01-25 17:02:14 -0700

    add img to review route mostly complete

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -48,10 +48,50 @@ router.get('/current', requireAuth, async (req, res) => {
 })
 
 
+router.post('/:reviewId/images', requireAuth, async (req, res) => {
+  //review must exist
+  let reviewExist = await Review.findByPk(req.params.reviewId);
+
+  if (!reviewExist) {
+    const err = new Error("Review couldn't be found");
+    err.status = 404;
+    return next(err);
+  }
+
+  //review must belong to the current user
+  let reviewUserMatch = await Review.findByPk(req.params.reviewId, {
+    where: { userId: req.user.id },
+  });
+
+  if (!reviewUserMatch) {
+    const err = new Error("Review must belong to current user");
+    err.status = 400;
+    return next(err);
+  }
+
+  //check if 10 images have already been added to this review
 
 
 
+  // add image to review (if none of the above checks are hit)
+  let reviewIdNum = Number(req.params.reviewId);
+  let newImg = await ReviewImage.create({
+    reviewId: reviewIdNum,
+    ...req.body,
+  });
 
+
+    let finalImg = newImg.toJSON()
+
+    delete finalImg.createdAt
+    delete finalImg.updatedAt;
+    delete finalImg.id;
+
+
+  res.json(finalImg)
+
+
+})
 
 
 module.exports = router;
