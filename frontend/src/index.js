897b2fa0f62b11b11e3569673868a47d7e247b60
Sean Baeyens <srbaeyens@gmail.com>
2023-02-10 11:32:42 -0600

    phase 1 - step 1 complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,15 +6,20 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from "./store/csrf";
+// ... other imports
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
+// ...
+
 
 if (process.env.NODE_ENV !== "production") {
   window.store = store;
