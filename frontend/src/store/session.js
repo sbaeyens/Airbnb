77cc463bddf7ab8b55b1846002847f1a64686058
Sean Baeyens <srbaeyens@gmail.com>
2023-02-10 11:53:30 -0600

    signup user thunk added

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -17,6 +17,23 @@ const removeUser = () => {
   };
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
