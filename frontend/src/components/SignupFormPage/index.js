50a73c17c3da4be1e699413c8d137c5bcf1114f7
Sean Baeyens <srbaeyens@gmail.com>
2023-02-10 16:11:36 -0600

    added modal for signup

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-import * as sessionActions from "../../store/session";
+// import * as sessionActions from "../../store/session";
+import * as sessionActions from '../../store/session'
 import "./SignupForm.css";
 
 function SignupFormPage() {
