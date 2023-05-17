0fc8f915096c00f541223049eb1a540f193050fa
Sean Baeyens <srbaeyens@gmail.com>
2023-05-17 13:06:24 -0700

    added maps to single spot page

--- frontend/src/components/MapView/index.js
+++ frontend/src/components/MapView/index.js
@@ -1,51 +1,75 @@
 import { Link } from "react-router-dom";
 import "./MapView.css";
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
-import { useRef, useState } from "react";
-import { NavLink } from "react-router-dom/cjs/react-router-dom";
+import { useRef, useState, useEffect } from "react";
+import { NavLink, useHistory } from "react-router-dom/cjs/react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllSpots } from "../../store/spots";
 
-function MapView() {
+function MapView({singleSpot}) {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const spots = useSelector((state) => {
+    return state.spots.allSpots;
+  });
+
+  console.log("singleSpot from mapcview", singleSpot)
+  useEffect(() => {
+    dispatch(getAllSpots());
+  }, [dispatch]);
+
+  console.log("state from Allspots component", spots);
+
   console.log("process.env", process.env);
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
+
+  if (!spots) {
+    return null;
+  }
+
+  // Put all spots in array
+  let spotsArr = Object.values(spots);
+  // console.log("spotsArr", spotsArr)
+  console.log("spotsArr from mapView", spotsArr);
 
   const containerStyle = {
     width: "400px",
     height: "400px",
   };
 
-  const center = { lat: 48.8584, lng: 2.2945 };
+  const center = { lat: singleSpot.lat, lng: singleSpot.lng };
 
   if (!isLoaded) {
     <div>LOADING...</div>;
   }
 
+
+
   return (
-    <div className="map-search-wrapper">
+    <div className="map-search-wrapper-viewer">
       {isLoaded ? (
-        <div className="google-maps-box">
+        <div className="google-maps-box-viewer">
           <GoogleMap
             center={center}
-            zoom={10}
+            zoom={12}
             mapContainerStyle={{ width: "100%", height: "100%" }}
           >
-            <Marker
-              position={center}
-              icon={"none"}
-              label={{
-                text: `hahafdfha`,
-                className: "marker-label",
-              }}
-            ></Marker>
+            {spots &&
+              spotsArr.map((spot) => (
+                <Marker
+                  position={{ lat: singleSpot.lat, lng: singleSpot.lng }}
+                  icon={"none"}
+                  label={{
+                    text: `$${spot.price.toString()}`,
+                    className: "marker-label",
+                  }}
+                ></Marker>
+              ))}
           </GoogleMap>
         </div>
       ) : null}
-      <NavLink to="/">
-        <div className="floating-map-toggle">
-          Show List <i className="fa-solid fa-map fa-show"></i>
-        </div>
-      </NavLink>
     </div>
   );
 }
